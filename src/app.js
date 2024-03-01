e2fa353fa078997200b2ac1e8e3fdc28afe2d8f6
Joelsv95 <157936456+Joelsv95@users.noreply.github.com>
2024-03-01 01:07:31 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,66 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var first = "A ";
+  var adj = [
+    "two headed ",
+    "flying ",
+    "angry ",
+    "lonely ",
+    "crazy ",
+    "glowing ",
+    "smelly ",
+    "slow ",
+    "old "
+  ];
+  var who = [
+    "jogger ",
+    "racoon ",
+    "dog ",
+    "merchant ",
+    "driver ",
+    "cat ",
+    "aliens "
+  ];
+  var action = [
+    "took my ",
+    "threw my ",
+    "yelled at my ",
+    "kicked my ",
+    "stole my ",
+    "burned my ",
+    "bit my ",
+    "hit my "
+  ];
+  var what = [
+    "toe ",
+    "car ",
+    "watch ",
+    "shoe ",
+    "wallet ",
+    "shirt ",
+    "keys ",
+    "computer ",
+    "phone ",
+    "sandwich "
+  ];
+  var where = [
+    "on the street",
+    "in my house",
+    "in my driveway",
+    "in front of the office",
+    "near the mall",
+    "near the toilet",
+    "at the bus station"
+  ];
+
+  var rdm1 = Math.floor(Math.random() * adj.length);
+  var rdm2 = Math.floor(Math.random() * who.length);
+  var rdm3 = Math.floor(Math.random() * action.length);
+  var rdm4 = Math.floor(Math.random() * what.length);
+  var rdm5 = Math.floor(Math.random() * where.length);
+
+  document.querySelector("#theexcuse").innerHTML =
+    first + adj[rdm1] + who[rdm2] + action[rdm3] + what[rdm4] + where[rdm5];
 };
